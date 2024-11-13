98117645a3bb8fe4748485827fdc6af9944c478f
1ce23ec017 <kv691551@gmail.com>
2024-11-13 15:22:20 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, target, found = 0;
 
     // Read the number of elements in the array
     printf("Enter the number of elements in the array: ");
@@ -16,12 +16,23 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    // Reverse the array
-    printf("Reversed array: ");
-    for (int i = n - 1; i >= 0; i--) {
-        printf("%d ", arr[i]);
+    // Read the target element to search for
+    printf("Enter the element to search: ");
+    scanf("%d", &target);
+
+    // Search for the target element in the array
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            printf("Element %d found at position %d.\n", target, i + 1);
+            found = 1; // Mark that the element was found
+            break;
+        }
     }
-    printf("\n");
+
+    // If the element is not found
+    if (!found) {
+        printf("Element %d not found in the array.\n", target);
+    }
 
     return 0;
 }
